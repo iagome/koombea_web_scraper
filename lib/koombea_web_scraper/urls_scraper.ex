ef21aa2aa188e55e833c8247757fd465fec6fdc7
iagome <iagomollon@gmail.com>
2024-06-03 19:54:08 -0300

    deal with errors and add doc

--- lib/koombea_web_scraper/urls_scraper.ex
+++ lib/koombea_web_scraper/urls_scraper.ex
@@ -3,11 +3,33 @@ defmodule KoombeaWebScraper.UrlsScraper do
   Responsible for scraping URLs
   """
 
+  require Logger
+
+  @doc """
+    Scrape any URL and parse the needed info
+
+    ## Examples
+
+        iex> scrape_and_parse_url("https://google.com")
+        {:ok, "Google", [%{url: url, name: name}]}
+
+        iex> scrape_and_parse_url(bad_url)
+        {:error, :error_fetching_url}
+  """
+  @spec scrape_and_parse_url(String.t()) :: {:ok, String.t(), list()} | {:error, :error_fetching_url} | any()
   def scrape_and_parse_url(url) do
     with %HTTPoison.Response{status_code: 200, body: body} <- Crawly.fetch(url),
          page_title <- parse_page_title!(body),
          components <- parse_components!(body) do
       {:ok, page_title, components}
+    else
+      %HTTPoison.Response{status_code: status, body: body} ->
+        Logger.error("#{__MODULE__} - Error fetching URL with status code: #{inspect(status)} and reason: #{inspect(body)}")
+        {:error, :error_fetching_url}
+
+      error ->
+        Logger.error("#{__MODULE__} - An unexpected error occurred: #{inspect(error)}")
+        error
     end
   end
 
