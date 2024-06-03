b7f14ec0daf923f1bf42abc1481ccc7d5a6cf7d3
iagome <iagomollon@gmail.com>
2024-06-03 19:53:53 -0300

    fix hostname

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,7 @@ config :bcrypt_elixir, :log_rounds, 1
 config :koombea_web_scraper, KoombeaWebScraper.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "koombea_web_scraper_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
